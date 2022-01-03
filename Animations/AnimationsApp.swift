e4f69d557e0e7925a201c8303661e2ffeaa4a2c1
cspeloso <chrispeloso2@gmail.com>
2022-01-03 18:30:07 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Chris Peloso on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
